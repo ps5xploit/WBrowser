5b51136d1bfde9698309b2cd07fe1ed1f84ea0de
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2023-12-17 11:22:49 +0100

    Add files via upload

--- custom_host_stuff.js
+++ custom_host_stuff.js
@@ -102,13 +102,11 @@ function redirectorGo() {
 
 const default_pinned_websites = [
     "https://google.com"
-	"https://ps5xploit.github.io"
-	"http://192.168.1.37:8080/"
 ]
 
 const dummy_history = [
     "https://google.com",
-    "https://ps5xploit.github.io",
+    "https://ps5jb.pages.dev",
     "https://github.com",
     "https://duckduckgo.com",
     "https://youtube.com",
