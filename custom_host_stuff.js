fed524895fecbf2958ff7cad9ad676596dae6f84
Mouuu㋡ <119304810+ps5xploit@users.noreply.github.com>
2025-02-21 20:53:39 +0100

    Update custom_host_stuff.js

--- custom_host_stuff.js
+++ custom_host_stuff.js
@@ -102,7 +102,7 @@ function redirectorGo() {
 
 const default_pinned_websites = [
     "https://google.com",
-    "https://ps5xploit.github.io/",
+    "https://ps5xploit.github.io",
     "https://ps5xploit-umtx.pages.dev",
 ]
 
